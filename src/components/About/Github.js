9b02f7ace9b28be82c33c0001efb81e26d9153d8
The1Don1 <mathendam@gmail.com>
2024-01-15 19:35:25 +0200

    changed github contributions theme

--- src/components/About/Github.js
+++ src/components/About/Github.js
@@ -1,4 +1,10 @@
-import React from "react";
+import React, {
+  DOMAttributes,
+  HTMLAttributes,
+  JSXElementConstructor,
+  ReactElement,
+  SVGAttributes,
+} from 'react';
 import GitHubCalendar from "react-github-calendar";
 import { Row } from "react-bootstrap";
 
@@ -8,14 +14,26 @@ function Github() {
       <h1 className="project-heading" style={{ paddingBottom: "20px" }}>
         Days I <strong className="purple">Code</strong>
       </h1>
-      <GitHubCalendar
+      {/* <GitHubCalendar
         username="The1Don1"
         blockSize={15}
         blockMargin={7}
         // theme = "dark"
-        color="#5f76f8"
+        colorScheme="dark"
         fontSize={16}
-      />
+      /> */}
+      <GitHubCalendar
+        username="The1Don1"
+        blockSize={14}
+        blockRadius={7}
+        blockMargin={5}
+        fontSize={16}
+        colorScheme="dark"
+  theme={{
+    light: ['#f0f0f0', '#c4edde', '#7ac7c4', '#f73859', '#384259'],
+    dark: ['#383838', '#4D455D', '#7DB9B6', '#F5E9CF', '#E96479'],
+  }}
+/>
     </Row>
   );
 }
