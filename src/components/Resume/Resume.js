870fb916605cedf76fa0d019ec5b80a71694a87b
The1Don1 <mathendam@gmail.com>
2024-02-03 13:27:48 +0200

    added dynamic views for android/tablet, fixed resume

--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
-import pdf from "../../Assets/../Assets/Mathenda_Maphasha_-_CV[1].pdf";
+import pdf from "../../Assets/../Assets/Mathenda Maphasha - CV[1].pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
@@ -31,8 +31,11 @@ function Resume() {
 
         <Row className="resume">
           <Document file={pdf} className="d-flex justify-content-center">
-            <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
+            <Page style="A4" pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
+            
+            <Page wrap={false} style={"A4"} pageNumber={2} scale={width > 786 ? 1.7 : 0.6} />
           </Document>
+          
         </Row>
 
         <Row style={{ justifyContent: "center", position: "relative" }}>
